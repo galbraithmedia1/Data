26062a36cce0ed85913463a3b31a2a93a98a0249
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-11 09:22:06 -0700

    setup done

---
+++ client/main.js
@@ -0,0 +1,10 @@
+
+
+
+ document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
