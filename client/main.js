9e254e0e68b1d27102d90f380d36c2429ace3ccf
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-11 09:29:11 -0700

    first feature done

--- client/main.js
+++ client/main.js
@@ -8,3 +8,11 @@
           alert(data);
         });
   };
+
+ document.getElementById("fortuneButton").onclick = function () {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
--- server/index.js
+++ server/index.js
@@ -8,10 +8,29 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+
+//Compliment Button 
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
 					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+//Fortune Button
+app.get("/api/fortune", (req, res) => {
+  const compliments = ["Those who care will make the effort.",
+					 "Today, your mouth might be moving but no one is listening.",
+					 "We first make our habits, and then our habits make us.",
+           "Wish you happiness.",
+           "You always bring others happiness."
   ];
 
   // choose random compliment
